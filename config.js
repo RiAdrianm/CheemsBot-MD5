7edc73ef6406a35aabb7d861839c4f8024682a1f
Ri <103224046+RiAdrianm@users.noreply.github.com>
2022-11-10 13:07:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,26 +14,26 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['6285161888973'] //ur owner number
+global.ownername = "Ri" //ur owner name
+global.ytname = "Gapunya" //ur yt chanel name
+global.socialm = "GitHub: Riadrianm" //ur github or insta name
+global.location = "WAKANDA FOREVAHH" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213'] //ur number
-global.ownernomer = "916909137213" //ur number
-global.ownertag = '916909137213' //ur tag number
-global.premium = ['916909137213'] //ur premium number
-global.botname = 'Cheems Bot V5' //ur bot name
-global.ownername = "🦄Dream Guy Xeon"
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
+global.owner = ['6285161888973'] //ur number
+global.ownernomer = "6285161888973" //ur number
+global.ownertag = '6285161888973' //ur tag number
+global.premium = ['6285161888973'] //ur premium number
+global.botname = 'Miuna V2' //ur bot name
+global.ownername = "Ri Gans"
+global.linkz = "www.facebook.com/Riadrianm" //your theme url which will be displayed on whatsapp
+global.websitex = "www.facebook.com/Riadrianm" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD5' //script link
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Sticker By" //ur sticker watermark packname
-global.author = "Cheems Bot" //ur sticker watermark author
-global.wm = "Xeon Bot Inc." //ur watermark
+global.themeemoji = "🥵" //ur theme emoji
+global.packname = "Sticker By Ri" //ur sticker watermark packname
+global.author = "Miuna V2" //ur sticker watermark author
+global.wm = "Ri" //ur watermark
 
 // Other
 global.sessionName = 'session'
